fe0e0e11f91abe9635ff7cfee5f9461f9f976426
Clinton Mutinda <mutinda1993@gmail.com>
2022-11-08 16:02:23 +0300

    Added preloader to the About view

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Preloader from '../../utils/preloader';
+import  $  from 'jquery';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  loadingView:boolean = false;
+  imagesLoaded:boolean = false;
+  siteImages:any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.loadingView = true;
+
+    $('html, body').animate({
+      scrollTop: $(".banner-image-section").offset({
+        top: 0
+      })
+    }, 500);
+  }
+
+  ngAfterViewInit():void{
+
+    this.siteImages = Preloader.getImages();
+    
   }
 
 }
