6c8ca42c9a291eb1f072c04df7abfa4e41a3ec11
Clinton Mutinda <mutinda1993@gmail.com>
2025-10-02 12:41:40 +0300

    Updated preloader and added scrolltop to app-component wrapper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'nerd-interpreter-ui';
+
+  constructor(
+    private router: Router,
+    private viewportScroller: ViewportScroller,
+  ) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.viewportScroller.scrollToPosition([0, 0]);
+    });
+  }
 }
